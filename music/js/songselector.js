155ddfd0ab08b1b3f581e47e5dc1f00d6d37158d
Kirill Romanov <djaler1@gmail.com>
2018-04-27 19:56:53 +0300

    Перемешивание песен при открытии страницы вместо постоянного случайного выбора

--- music/js/songselector.js
+++ music/js/songselector.js
@@ -1,38 +1,33 @@
-'use strict';
-
-const prefix = './audio/';
-
-const songs = [
-
-  'Frederic Chopin - A comme amour.mp3',
-  'Blonde Redhead - For The Damaged Coda.mp3'
-
-];
-
-function getRandomTrack() {
-
-  return songs[Math.floor(Math.random() * songs.length)];
-
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
 }
 
-if (!((document.createElement("audio").play) ? true : false)) {
+const prefix = './audio';
 
-  alert('Здесь должна быть музыка, но ты - некрофил.');
+const songs = [
+    'Frederic Chopin - A comme amour.mp3',
+    'Blonde Redhead - For The Damaged Coda.mp3'
+];
 
+shuffle(songs);
+
+if (!document.createElement("audio").play) {
+    alert('Здесь должна быть музыка, но ты - некрофил. Поставь уже настоящий браузер');
 } else {
+    let i = 0;
+    let track = songs[i % songs.length];
 
-  const player = new Audio(`${prefix}${getRandomTrack()}`);
-
-  player.play();
-
-  player.addEventListener('ended', () => {
-
-    player.src = `${prefix}${getRandomTrack()}`;
-
+    const player = new Audio(`${prefix}/${track}`);
     player.play();
 
-    return;
+    player.addEventListener('ended', () => {
+        track = songs[++i % songs.length];
 
-  });
-
+        player.src = `${prefix}/${track}`;
+        player.play();
+    });
 }
